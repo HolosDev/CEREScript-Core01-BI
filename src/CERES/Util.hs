861cef44b14e016fd590ea1a4584d214b9b1f74c
YongJoon Joe <developer@quietjoon.net>
2020-05-06 22:16:43 +0900

    Add util functions for Storage manipulation

---
+++ src/CERES/Util.hs
@@ -0,0 +1,26 @@
+module CERES.Util where
+
+
+import           Data.IntMap                   as IM
+import           Data.Trie.Text                as Trie
+
+import           Data.CERES.Data
+import           Data.CERES.Type
+import           CERES.BI.Data
+
+
+vMapInsert :: Idx -> Value -> ValueMap -> ValueMap
+vMapInsert = IM.insert
+vNMapInsert :: NKey -> Value -> ValueNMap -> ValueNMap
+vNMapInsert = Trie.insert
+
+vMapUpdate :: Idx -> Maybe Value -> ValueMap -> ValueMap
+vMapUpdate idx mValue = IM.update (const mValue) idx
+vNMapUpdate :: NKey -> Maybe Value -> ValueNMap -> ValueNMap
+vNMapUpdate nKey mValue vnMap =
+  maybe (Trie.delete nKey vnMap) (\v -> Trie.insert nKey v vnMap) mValue
+
+vMapLookup :: Idx -> ValueMap -> Maybe Value
+vMapLookup = IM.lookup
+vNMapLookup :: NKey -> ValueNMap -> Maybe Value
+vNMapLookup = Trie.lookup
