8a00beb92d854e2fd87930bef31afe8e9d547dcc
YongJoon Joe <developer@quietjoon.net>
2020-05-06 20:12:34 +0900

    Add utility functions for get/set a value

--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,18 @@
 module Util where
 
 
+import           Data.IntMap                   as IM
+import           Data.Trie.Text                as Trie
+
+
 fst3 (a, _, _) = a
 snd3 (_, b, _) = b
 trd3 (_, _, c) = c
+
+recover :: v -> Maybe v -> Maybe v
+recover whenFail Nothing = Just whenFail
+recover whenFail success = success
+
+vMapUpdate idx mValue = IM.update (const mValue) idx
+nvMapUpdate nKey mValue vnMap =
+  maybe (Trie.delete nKey vnMap) (\v -> Trie.insert nKey v vnMap) mValue
