f4cc39b44c97fedfbe2b6fd9c7b17dff7e679e81
YongJoon Joe <developer@quietjoon.net>
2020-05-18 23:36:11 +0900

    Add check function `exceedTS`

--- src/CERES/BI/Interpret/Script.hs
+++ src/CERES/BI/Interpret/Script.hs
@@ -2,7 +2,23 @@ module CERES.BI.Interpret.Script where
 
 
 import           Data.CERES.Data
+import           Data.CERES.Data.Method
+
+import           CERES.BI.Data
+import           CERES.BI.Data.Constants
+import           CERES.BI.Data.Environment
+import           CERES.BI.Data.Function
+import           CERES.BI.Util
 
 
 isScriptEnd :: CEREScript -> Bool
 isScriptEnd = null
+
+-- NOTE: exceedTime
+-- * Calculate theElapsedTime exceeds worldTSSize or not
+-- * Returns whether a sum of theElapsedTime and SI-ElapsedTime exceeds or not
+exceedTS :: Input -> Bool
+exceedTS anInput@(aWorld@World {..}, aSI@SI {..}, cState) =
+   theElapsedTime + siElapsedTime > worldTSSize
+ where
+  theElapsedTime = maybe 0 getInt $ vMapLookup elapsedInternalTimeIdx (lVars . lCache $ cState)
